a3bc459a9c35c90de9f3cf10eea82040942b2a9e
eantonova <antonova.hellokitty2011@yandex.ru>
2023-02-19 10:26:51 +0300

    Add HW 1

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
-export const Restaurant = () => {
-  return <div>Restaurant</div>;
+import { Menu } from "../Menu/Menu";
+import { Reviews } from "../Reviews/Reviews";
+
+export const Restaurant = ({ restaurant }) => {
+  const { name, menu, reviews } = restaurant;
+
+  return (
+    <div>
+      <h2>{ name }</h2>
+      <Menu menu={menu} />
+      <Reviews reviews={reviews} />
+    </div>
+  );
 };
